60118de674e8e63dfac3c40483ec69f19c3c7611
wave49192 <sirawich.tu@ku.th>
2022-04-26 22:54:15 +0700

    update dao

--- utils/dao/citizen_dao.py
+++ utils/dao/citizen_dao.py
@@ -3,8 +3,22 @@ from sqlalchemy.orm.session import Session
 
 
 class CitizenDao:
-  def __init__(self, session: Session):
-    self.__session = session
+    def __init__(self, session: Session):
+        self.__session = session
 
-  def get_all_cars(self):
-    return self.__session.query(CitizenModel).all()
+    def get_all_citizen(self):
+        return self.__session.query(CitizenModel).all()
+
+    def get_citizen_by_id(self, id):
+        return self.__session.query(CitizenModel).filter(CitizenModel.id == id)[0]
+
+    def get_citizen_by_firstname(self, firstname):
+        return self.__session.query(CitizenModel).filter(CitizenModel.firstname == firstname).all()
+
+    def get_citizen_by_lastname(self, lastname):
+        return self.__session.query(CitizenModel).filter(CitizenModel.lastname == lastname).all()
+
+    def create_new_citizen(self, citizen: CitizenModel):
+        self.__session.add(citizen)
+        self.__session.commit()
+        print("new citizen added to the database")
--- utils/dao/states_dao.py
+++ utils/dao/states_dao.py
@@ -3,8 +3,14 @@ from sqlalchemy.orm.session import Session
 
 
 class StatesDao:
-  def __init__(self, session: Session):
-    self.__session = session
+    def __init__(self, session: Session):
+        self.__session = session
 
-  def get_all_states(self):
-    return self.__session.query(StatesModel).all()
+    def get_all_states(self):
+        return self.__session.query(StatesModel).all()
+
+    def get_states_by_id(self, id):
+        return self.__session.query(StatesModel).filter(StatesModel.id == id)[0]
+
+    def get_states_by_abbreviation(self, word):
+        return self.__session.query(StatesModel).filter(StatesModel.abbreviation == word)[0]

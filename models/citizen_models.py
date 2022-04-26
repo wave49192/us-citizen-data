72cff81f8999a289be772637c306c5c5ec0523b9
wave49192 <sirawich.tu@ku.th>
2022-04-26 20:52:22 +0700

    add models

---
+++ models/citizen_models.py
@@ -0,0 +1,20 @@
+from sqlalchemy import Column, Integer, Text
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+
+class UserModel(Base):
+    __tablename__ = "citizens"
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    firstname = Column(Text, nullable=False)
+    lastname = Column(Text, nullable=False)
+    gender = Column(Text, nullable=False)
+    jobname = Column(Text, nullable=False)
+    annual_salary = Column(Integer, nullable=False)
+    state_id = Column(Integer, nullable=False)
+
+    def __repr__(self) -> str:
+        return f"<Citizen: (id={self.id},firstname={self.firstname},lastname={self.lastname},gender={self.gender}" \
+               f",jobname={self.jobname},annual_salary={self.annual_salary},state_id={self.state_id})>"
---
+++ models/state_models.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Integer, Text
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+
+class UserModel(Base):
+
+    __tablename__ = "citizens"
+    id = Column(Integer, primary_key=True, nullable=False)
+    state_name = Column(Text, nullable=False)
+    abbreviation = Column(Text, nullable=False)
+
+
+    def __repr__(self) -> str:
+        return f"<State: (id={self.id},firstname={self.state_name},abbr={self.abbreviation})>"

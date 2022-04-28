d58fd184209aad45a0638f3b234036545ac99744
wave49192 <sirawich.tu@ku.th>
2022-04-28 10:58:21 +0700

    add dao fucntion

--- main.py
+++ main.py
@@ -7,11 +7,15 @@ print(us_citizen.citizen().get_all_citizen())
 print(us_citizen.citizen().get_citizen_by_id(3))
 print(us_citizen.citizen().get_citizen_by_firstname("Angelica"))
 print(us_citizen.citizen().get_citizen_by_lastname("Emerson"))
+print(us_citizen.citizen.get_citizen_by_salary_range(10000, 20000))
+print(us_citizen.citizen.get_citizen_by_gender("Male"))
+print(us_citizen.citizen.get_citizen_by_state_id(32))
+
 print(us_citizen.state().get_all_states())
 print(us_citizen.state().get_states_by_id(3))
 print(us_citizen.state().get_states_by_abbreviation("AR"))
 
-new_citizen = CitizenModel(id="160", firstname="Sirawich", lastname="Tumtamai", gender="Male",
-                           jobname="Student", annual_salary=50000, state_id=23)
+# new_citizen = CitizenModel(id="160", firstname="Sirawich", lastname="Tumtamai", gender="Male",
+#                            jobname="Student", annual_salary=50000, state_id=23)
 
-us_citizen.citizen().create_new_citizen(new_citizen)
+# us_citizen.citizen().create_new_citizen(new_citizen)
--- utils/dao/citizen_dao.py
+++ utils/dao/citizen_dao.py
@@ -18,6 +18,16 @@ class CitizenDao:
     def get_citizen_by_lastname(self, lastname):
         return self.__session.query(CitizenModel).filter(CitizenModel.lastname == lastname).all()
 
+    def get_citizen_by_salary_range(self, first, second):
+        return self.__session.query(CitizenModel).filter(CitizenModel.annual_salary >= first and
+                                                         CitizenModel.annual_salary <= second).all()
+
+    def get_citizen_by_gender(self, gender):
+        return self.__session.query(CitizenModel).filter(CitizenModel.gender == gender).all()
+
+    def get_citizen_by_state_id(self, state_id):
+        return self.__session.query(CitizenModel).filter(CitizenModel.state_id == state_id).all()
+
     def create_new_citizen(self, citizen: CitizenModel):
         self.__session.add(citizen)
         self.__session.commit()
